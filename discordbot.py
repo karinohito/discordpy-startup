96a651e94c61df8579eae8ed01e9a2839a1034df
karinohito <63297996+karinohito@users.noreply.github.com>
2020-08-16 15:20:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx..disconnect()
     
 @bot.event
 async def on_message(message):
